5f323810b923ea3d989b94e253054439dce3a0b5
Bartlomiej Duda <ikskoks@gmail.com>
2025-06-29 20:17:47 +0200

    v0.33.0

--- src/EA_Image/ea_image_encoder.py
+++ src/EA_Image/ea_image_encoder.py
@@ -3,13 +3,12 @@ Copyright © 2024-2025  Bartłomiej Duda
 License: GPL-3.0 License
 """
 
-from PIL import Image
 from reversebox.common.common import fill_data_with_padding_to_desired_length
 from reversebox.common.logger import get_logger
 from reversebox.compression.compression_refpack import RefpackHandler
+from reversebox.image.common import get_linear_image_data_size
 from reversebox.image.image_encoder import ImageEncoder
 from reversebox.image.image_formats import ImageFormats
-from reversebox.image.pillow_wrapper import PillowWrapper
 from reversebox.image.swizzling.swizzle_ps2 import swizzle_ps2_palette
 
 from src.EA_Image.common import (
@@ -26,7 +25,6 @@ from src.EA_Image.common_ea_dir import (
 from src.EA_Image.constants import IMPORT_IMAGES_SUPPORTED_TYPES
 from src.EA_Image.dir_entry import DirEntry
 from src.EA_Image.dto import EncodeInfoDTO, PaletteInfoDTO, PartialEncodeInfoDTO
-from src.EA_Image.ea_image_decoder import decode_image_data_by_entry_type
 from src.EA_Image.ea_image_main import EAImage
 
 logger = get_logger(__name__)
@@ -34,72 +32,82 @@ logger = get_logger(__name__)
 
 def encode_ea_image(rgba8888_data: bytes, ea_dir: DirEntry, ea_img: EAImage) -> EncodeInfoDTO:
     logger.info("Initializing encode_ea_image")
-    entry_type: int = ea_dir.h_record_id
+    entry_type: int = ea_dir.h_record_id & 0x7F
     indexed_image_format: ImageFormats = get_indexed_image_format(get_bpp_for_image_type(entry_type))
     palette_info_dto: PaletteInfoDTO = get_palette_info_dto_from_dir_entry(ea_dir, ea_img)
     palette_format: ImageFormats = get_indexed_palette_format(palette_info_dto.entry_id, len(palette_info_dto.data))
-    final_encoded_image_data: bytes = b""
 
     if entry_type not in IMPORT_IMAGES_SUPPORTED_TYPES:
         raise Exception("Image type not supported for encoding!")
 
-    partial_main_image_info: PartialEncodeInfoDTO = encode_image_data_by_entry_type(
-        entry_type, rgba8888_data, ea_dir.h_width, ea_dir.h_height, indexed_image_format, palette_format
+    # encode logic (main + mipmaps)
+    partial_image_info: PartialEncodeInfoDTO = encode_image_data_by_entry_type(
+        entry_type,
+        rgba8888_data,
+        ea_dir.h_width,
+        ea_dir.h_height,
+        indexed_image_format,
+        palette_format,
+        ea_dir.h_mipmaps_count,
     )
-    raw_encoded_image_data: bytes = partial_main_image_info.encoded_image_data
 
+    # swizzle logic
     if is_image_swizzled(ea_dir):
-        partial_main_image_info.encoded_image_data = handle_image_swizzle_logic(
-            partial_main_image_info.encoded_image_data, entry_type, ea_dir.h_width, ea_dir.h_height, ea_img.sign, True
+        start_offset: int = 0
+        image_size: int = get_linear_image_data_size(ea_dir.h_image_bpp, ea_dir.h_width, ea_dir.h_height)
+        end_offset: int = image_size
+        swizzled_image_data = handle_image_swizzle_logic(
+            partial_image_info.encoded_image_data[start_offset:end_offset],
+            entry_type,
+            ea_dir.h_width,
+            ea_dir.h_height,
+            ea_img.sign,
+            True,
         )
 
-    final_encoded_image_data += partial_main_image_info.encoded_image_data
-    final_encoded_palette_data = partial_main_image_info.encoded_palette_data
+        # mipmaps swizzle logic
+        if ea_dir.h_mipmaps_count and ea_dir.h_mipmaps_count > 0:
+            mip_width: int = ea_dir.h_width
+            mip_height: int = ea_dir.h_height
+            for i in range(ea_dir.h_mipmaps_count):
+                mip_width //= 2
+                mip_height //= 2
+                mipmap_size = get_linear_image_data_size(ea_dir.h_image_bpp, mip_width, mip_height)
+                start_offset = end_offset
+                end_offset = start_offset + mipmap_size
 
-    # handle mipmaps
-    # TODO - not working! Should be handled by ReverseBox instead?
-    decoded_main_tex_data: bytes = decode_image_data_by_entry_type(
-        entry_type, raw_encoded_image_data, palette_info_dto, ea_dir
-    )
-    base_img: Image = PillowWrapper().get_pillow_image_from_rgba8888_data(
-        decoded_main_tex_data, ea_dir.h_width, ea_dir.h_height
-    )
-    mip_width: int = ea_dir.h_width
-    mip_height: int = ea_dir.h_height
-    for i in range(ea_dir.h_mipmaps_count):
-        mip_width //= 2
-        mip_height //= 2
-        mip_pillow_img: Image = base_img.resize((mip_width, mip_height))
-        mip_rgba_data: bytes = PillowWrapper().get_image_data_from_pillow_image(mip_pillow_img)
+                mip_swizzled_data = handle_image_swizzle_logic(
+                    partial_image_info.encoded_image_data[start_offset:end_offset],
+                    entry_type,
+                    mip_width,
+                    mip_height,
+                    ea_img.sign,
+                    True,
+                )
+                swizzled_image_data += mip_swizzled_data
 
-        partial_mipmap_info: PartialEncodeInfoDTO = encode_image_data_by_entry_type(
-            entry_type, mip_rgba_data, mip_width, mip_height, indexed_image_format, palette_format
-        )
-        partial_mipmap_info.encoded_image_data = handle_image_swizzle_logic(
-            partial_mipmap_info.encoded_image_data, entry_type, mip_width, mip_height, ea_img.sign, True
-        )
+        partial_image_info.encoded_image_data = swizzled_image_data
 
-        final_encoded_image_data += partial_mipmap_info.encoded_image_data
-
+    # compression logic
     if is_image_compressed(entry_type):
-        final_encoded_image_data = RefpackHandler().compress_data(final_encoded_image_data)
+        partial_image_info.encoded_image_data = RefpackHandler().compress_data(partial_image_info.encoded_image_data)
 
-    if len(final_encoded_image_data) > len(ea_dir.raw_data):
+    if len(partial_image_info.encoded_image_data) > len(ea_dir.raw_data):
         raise Exception("Encoded data too big!")
 
-    if len(final_encoded_image_data) < len(ea_dir.raw_data):
-        final_encoded_image_data = fill_data_with_padding_to_desired_length(
-            final_encoded_image_data, len(ea_dir.raw_data)
+    if len(partial_image_info.encoded_image_data) < len(ea_dir.raw_data):
+        partial_image_info.encoded_image_data = fill_data_with_padding_to_desired_length(
+            partial_image_info.encoded_image_data, len(ea_dir.raw_data)
         )
 
     if palette_info_dto.swizzle_flag:
-        final_encoded_palette_data = swizzle_ps2_palette(final_encoded_palette_data)
+        partial_image_info.encoded_palette_data = swizzle_ps2_palette(partial_image_info.encoded_palette_data)
 
     return EncodeInfoDTO(
-        encoded_img_data=final_encoded_image_data,
-        encoded_palette_data=final_encoded_palette_data,
+        encoded_img_data=partial_image_info.encoded_image_data,
+        encoded_palette_data=partial_image_info.encoded_palette_data,
         palette_entry_id=palette_info_dto.entry_id,
-        is_palette_imported_flag=True if len(final_encoded_palette_data) > 0 else False,
+        is_palette_imported_flag=True if len(partial_image_info.encoded_palette_data) > 0 else False,
     )
 
 
@@ -110,81 +118,152 @@ def encode_image_data_by_entry_type(
     img_height: int,
     indexed_image_format: ImageFormats,
     palette_format: ImageFormats,
+    mipmaps_count: int,
 ) -> PartialEncodeInfoDTO:
     image_encoder = ImageEncoder()
     encoded_palette_data: bytes = b""
 
     if entry_type == 1:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=16
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=16,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 2:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=256
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=256,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 3:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.RGBA5551)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.RGBA5551, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 4:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.RGB888)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.RGB888, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 5:
         encoded_image_data = rgba8888_data
     elif entry_type == 22:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.ARGB8888)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.ARGB8888, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 64:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=16
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=16,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 65:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=256
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=256,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 66:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.RGBT5551)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.RGBT5551, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type in (88, 89):
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.RGB565)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.RGB565, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 90:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.RGBX4444)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.RGBX4444, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 91:
         encoded_image_data = rgba8888_data
     elif entry_type == 92:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=16
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=16,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 93:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=256
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=256,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 96:
         encoded_image_data = image_encoder.encode_compressed_image(
-            rgba8888_data, img_width, img_height, ImageFormats.BC1_DXT1
+            rgba8888_data, img_width, img_height, ImageFormats.BC1_DXT1  # TODO - mipmaps
         )
     elif entry_type == 97:
         encoded_image_data = image_encoder.encode_compressed_image(
-            rgba8888_data, img_width, img_height, ImageFormats.BC2_DXT3
+            rgba8888_data, img_width, img_height, ImageFormats.BC2_DXT3  # TODO - mipmaps
         )
     elif entry_type == 98:
         encoded_image_data = image_encoder.encode_compressed_image(
-            rgba8888_data, img_width, img_height, ImageFormats.BC3_DXT5
+            rgba8888_data, img_width, img_height, ImageFormats.BC3_DXT5  # TODO - mipmaps
         )
     elif entry_type == 109:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.BGRA4444)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.BGRA4444, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 120:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.BGR565)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.BGR565, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 121:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=16
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=16,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 123:
         encoded_image_data, encoded_palette_data = image_encoder.encode_indexed_image(
-            rgba8888_data, img_width, img_height, indexed_image_format, palette_format, max_color_count=256
+            rgba8888_data,
+            img_width,
+            img_height,
+            indexed_image_format,
+            palette_format,
+            max_color_count=256,
+            number_of_mipmaps=mipmaps_count,
         )
     elif entry_type == 125:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.BGRA8888)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.BGRA8888, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 126:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.BGRA5551)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.BGRA5551, number_of_mipmaps=mipmaps_count
+        )
     elif entry_type == 127:
-        encoded_image_data = image_encoder.encode_image(rgba8888_data, img_width, img_height, ImageFormats.BGR888)
+        encoded_image_data = image_encoder.encode_image(
+            rgba8888_data, img_width, img_height, ImageFormats.BGR888, number_of_mipmaps=mipmaps_count
+        )
     else:
-        raise Exception("Image type not supported for encoding!")
+        raise Exception(f"Image type {entry_type} not supported for encoding!")
 
     return PartialEncodeInfoDTO(encoded_image_data=encoded_image_data, encoded_palette_data=encoded_palette_data)
--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from reversebox.common.logger import get_logger
 
 from src.GUI.GUI_main import EAManGui
 
-VERSION_NUM = "v0.32.0"
+VERSION_NUM = "v0.33.0"
 
 logger = get_logger("main")
 
