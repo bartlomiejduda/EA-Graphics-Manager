d918be59ce2f17a6f76ff8a922611d16bab36109
Bartlomiej Duda <ikskoks@gmail.com>
2026-01-17 18:32:03 +0100

    v0.43.1 - fix decoding for PAL4 2IA format

--- src/EA_Image/ea_image_decoder.py
+++ src/EA_Image/ea_image_decoder.py
@@ -167,6 +167,7 @@ def decode_image_data_by_entry_type(
             ea_dir_entry.h_height,
             get_indexed_image_format(get_bpp_for_image_type(entry_type)),
             get_indexed_palette_format(palette_info_dto.entry_id, len(palette_info_dto.data)),
+            image_endianess="big",
             palette_endianess="big",
         )
     elif entry_type == 25:
@@ -177,6 +178,7 @@ def decode_image_data_by_entry_type(
             ea_dir_entry.h_height,
             get_indexed_image_format(get_bpp_for_image_type(entry_type)),
             get_indexed_palette_format(palette_info_dto.entry_id, len(palette_info_dto.data)),
+            image_endianess="big",
             palette_endianess="big",
         )
     elif entry_type == 30:
